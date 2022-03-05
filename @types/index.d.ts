2e33bb8c70a6de796a741d33ff1662e66e2a5375
Matthew Sells <matthew.m.sells@gmail.com>
2022-03-05 07:15:09 -0600

    update global types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,19 +1,3 @@
-import { IronSession } from 'iron-session';
-
-import type { User } from '@/types/user';
-
-// interface IronSessionData {
-//   user: Nullable<User>;
-// }
-
-// declare module 'http' {
-//   interface IncomingMessage {
-//     session: {
-//       user: Nullable<User>;
-//     } & IronSession;
-//   }
-// }
-
 // Global types
 declare global {
   type Nullable<T> = T | null;
