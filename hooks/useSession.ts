692a3b5e4fd2e3b8914b70e7c50ae9bd33ffcd71
Matthew Sells <matthew.m.sells@gmail.com>
2022-03-08 18:31:40 -0600

    add redirects to hooks

--- hooks/useSession.ts
+++ hooks/useSession.ts
@@ -1,11 +1,28 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import useSWR from 'swr';
 
 import http, { Routes } from '@/lib/http';
+import { Pages } from '@/lib/pages';
+
+import { usePrevious } from './usePrevious';
 
 export default function useSession() {
+  const router = useRouter();
+
   const { data: user, mutate: setUser } = useSWR(Routes.Sessions, () =>
     http.get(Routes.Sessions).then((res) => res.user)
   );
+
+  const prevUser = usePrevious(user);
+
+  useEffect(() => {
+    if (user && !prevUser) {
+      router.push(Pages.Campaigns);
+    } else if (!user && prevUser) {
+      router.push(Pages.Login);
+    }
+  }, [user, prevUser, router]);
 
   return {
     isLoggedIn: !!user,
---
+++ lib/pages/pages.ts
@@ -0,0 +1,4 @@
+export enum Pages {
+  Campaigns = '/campaigns',
+  Login = '/login',
+}
